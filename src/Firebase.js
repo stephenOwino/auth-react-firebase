df4eabacd3be036165a4c717ffcf78e52e8393c9
stephenOwino <stephenowin233@gmail.com>
2024-08-05 10:30:52 +0300

    firebase credentials

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,18 +1,16 @@
-
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth'
-
-
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAHQ9BygNZnHow7b0uk5jCjGRTsVFQiZnQ",
-  authDomain: "fir-auth-yt-5eaa5.firebaseapp.com",
-  projectId: "fir-auth-yt-5eaa5",
-  storageBucket: "fir-auth-yt-5eaa5.appspot.com",
-  messagingSenderId: "617373827366",
-  appId: "1:617373827366:web:714a00dec07f6a656e41e7"
+	apiKey: "AIzaSyBMJR_CGMvj2JzP-mWZMUuGmjpwwvgVNC8",
+	authDomain: "auth-firebase-withreact.firebaseapp.com",
+	projectId: "auth-firebase-withreact",
+	storageBucket: "auth-firebase-withreact.appspot.com",
+	messagingSenderId: "328574177233",
+	appId: "1:328574177233:web:fe2a1913201d024b565af0",
+	measurementId: "G-6HRYM4PCE8",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
+export const auth = getAuth(app);
